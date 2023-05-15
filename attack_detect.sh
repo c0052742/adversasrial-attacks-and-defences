6fd46dc57d046502d6a3a75f6bb2cd66691203de
Ignas Vaicius <i.vaicius1@newcastle.ac.uk>
2023-05-15 20:41:13 +0100

    remove unneeded files

--- attack_detect.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-python detect.py --weights 'runs/train/KITTI_med_evolve/weights/best.pt' --source 'adversarial_images/fgm' --name fgm
-python detect.py --weights 'runs/train/KITTI_med_evolve/weights/best.pt' --source 'adversarial_images/dpt' --name dpt       
-python detect.py --weights 'runs/train/KITTI_med_evolve/weights/best.pt' --source 'adversarial_images/pgd' --name pgd
